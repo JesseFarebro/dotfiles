26d174a8850de7b68e57b6e7a75b5522822ef1b4
Jesse Farebrother <jessefarebro@gmail.com>
2022-08-09 11:03:59 -0400

    Only install fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,16 @@
-if status --is-interactive
-  fish_vi_key_bindings
-
-  # Bootstrap session
-  if not functions -q fisher
-      curl -sL https://git.io/fisher | source \
-        and fisher update
-  end
-end
+# if status --is-interactive
+#   # fish_vi_key_bindings
+#
+#   # Bootstrap session
+#   # if not functions -q fisher
+#   #     curl -sL https://git.io/fisher | source \
+#   #       and fisher update
+#   # end
+#
+#   # # >>> conda initialize >>>
+#   # # !! Contents within this block are managed by 'conda init' !!
+#   # eval /usr/local/Caskroom/mambaforge/base/bin/conda "shell.fish" "hook" $argv | source
+#   # # <<< conda initialize <<<
+# end
+#
+#
