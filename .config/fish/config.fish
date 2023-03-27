fe808ed7237b94c806fe276cefee4c7522863e50
Jesse Farebrother <jessefarebro@gmail.com>
2023-03-27 14:22:05 -0400

    op completions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,24 @@
 if status --is-interactive
   fish_vi_key_bindings
 
-  # Bootstrap session
+  # Bootstrap fisher
   if not functions -q fisher
       curl -sL https://git.io/fisher | source \
         and fisher update
   end
 
+  # Mamba
   if type -q micromamba; and set -q MAMBA_ROOT_PREFIX; and set -q MAMBA_EXE
     $MAMBA_EXE shell hook --shell fish --prefix $MAMBA_ROOT_PREFIX | source
   end
 
-  if type -q zoxide
-    zoxide init fish | source
-  end
+  # Zoxide
+  type -q zoxide; and zoxide init fish | source
 
+  # 1Password completions
+  type -q op; and op completion fish | source
+
+  # Starship
   if type -q starship
     starship init fish | source
     enable_transience
