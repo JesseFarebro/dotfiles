a91a94d905071ba51d480cdc16bc6f3f29d384aa
Jesse Farebrother <jessefarebro@gmail.com>
2023-11-17 16:05:24 -0500

    dfi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,12 @@ if status --is-interactive
 
     fish_vi_key_bindings
 
+    # Bootstrap fisher
+    if not functions -q fisher
+        curl -sL https://git.io/fisher | source
+        and fisher update
+    end
+
     # Mamba
     if type -q micromamba; and set -q MAMBA_ROOT_PREFIX; and set -q MAMBA_EXE
         $MAMBA_EXE shell hook --shell fish --prefix $MAMBA_ROOT_PREFIX | source
